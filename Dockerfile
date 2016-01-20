3b446bcddc4417e02d7efaa9d3ec5625e2045932
madesst <madesst@gmail.com>
2016-01-20 15:24:28 +0000

    Switch to 9.0 and lot of another changes

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
 FROM ubuntu:trusty
 
-MAINTAINER Wurstmeister 
+MAINTAINER madesst 
 
-RUN apt-get update && apt-get install -y unzip openjdk-6-jdk wget curl git docker.io jq
+ENV KAFKA_VERSION="0.9.0.0" SCALA_VERSION="2.11"
 
-ENV KAFKA_VERSION="0.8.2.2" SCALA_VERSION="2.10"
+RUN apt-get update && apt-get install -y unzip openjdk-7-jdk wget curl git jq
+
 ADD download-kafka.sh /tmp/download-kafka.sh
 RUN /tmp/download-kafka.sh
 RUN tar xf /tmp/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz -C /opt
@@ -14,6 +15,4 @@ VOLUME ["/kafka"]
 ENV KAFKA_HOME /opt/kafka_${SCALA_VERSION}-${KAFKA_VERSION}
 ADD start-kafka.sh /usr/bin/start-kafka.sh
 ADD broker-list.sh /usr/bin/broker-list.sh
-
-# Use "exec" form so that it runs as PID 1 (useful for graceful shutdown)
-CMD ["start-kafka.sh"]
+CMD start-kafka.sh
--- start-kafka.sh
+++ start-kafka.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-if [[ -z "$KAFKA_ADVERTISED_PORT" ]]; then
-    export KAFKA_ADVERTISED_PORT=$(docker port `hostname` 9092 | sed -r "s/.*:(.*)/\1/g")
-fi
+host=$(hostname --ip-address | sed 's/\([0-9\.]*\).*/\1/')
+
 if [[ -z "$KAFKA_BROKER_ID" ]]; then
-    export KAFKA_BROKER_ID=$(docker inspect `hostname` | jq --raw-output '.[0] | .Name' | awk -F_ '{print $3}')
+    ip_digitals=$(echo $host | tr -d '.')
+    export KAFKA_BROKER_ID=$(($ip_digitals+$RANDOM)) #random need in case with multiple brokers at single host
 fi
 if [[ -z "$KAFKA_LOG_DIRS" ]]; then
     export KAFKA_LOG_DIRS="/kafka/kafka-logs-$KAFKA_BROKER_ID"
 fi
 if [[ -z "$KAFKA_ZOOKEEPER_CONNECT" ]]; then
-    export KAFKA_ZOOKEEPER_CONNECT=$(env | grep ZK.*PORT_2181_TCP= | sed -e 's|.*tcp://||' | paste -sd ,)
+    export KAFKA_ZOOKEEPER_CONNECT="$(env | grep ZK.*PORT_2181_TCP= | sed -e 's|.*tcp://||' | paste -sd ,)"
 fi
 
 if [[ -n "$KAFKA_HEAP_OPTS" ]]; then
@@ -18,8 +18,8 @@ if [[ -n "$KAFKA_HEAP_OPTS" ]]; then
     unset KAFKA_HEAP_OPTS
 fi
 
-if [[ -z "$KAFKA_ADVERTISED_HOST_NAME" && -n "$HOSTNAME_COMMAND" ]]; then
-    export KAFKA_ADVERTISED_HOST_NAME=$(eval $HOSTNAME_COMMAND)
+if [[ -z "$KAFKA_ADVERTISED_HOST_NAME" ]]; then
+    export KAFKA_ADVERTISED_HOST_NAME=$host
 fi
 
 for VAR in `env`
@@ -35,19 +35,7 @@ do
   fi
 done
 
-# Capture kill requests to stop properly
-trap "$KAFKA_HOME/bin/kafka-server-stop.sh; echo 'Kafka stopped.'; exit" SIGHUP SIGINT SIGTERM
-
 $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties &
 KAFKA_SERVER_PID=$!
-
-while netstat -lnt | awk '$4 ~ /:9092$/ {exit 1}'; do sleep 1; done
-
-if [[ -n $KAFKA_CREATE_TOPICS ]]; then
-    IFS=','; for topicToCreate in $KAFKA_CREATE_TOPICS; do
-        IFS=':' read -a topicConfig <<< "$topicToCreate"
-        $KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper $KAFKA_ZOOKEEPER_CONNECT --replication-factor ${topicConfig[2]} --partition ${topicConfig[1]} --topic "${topicConfig[0]}"
-    done
-fi
 
 wait $KAFKA_SERVER_PID
